62d8f005dc0d5aa678309f92fd40245da3706b6a
nabilapasha <nabilaislamiah69@gmail.com>
2022-11-02 12:25:09 -0500

    Nabila

---
+++ index.js
@@ -0,0 +1,117 @@
+// var a = 12 
+
+// console.log(a);
+
+// console.log("hello nabila");
+
+// var bil1 = 11;
+// var bil2 = 12;
+
+// var hasil = (bil1 + bil2);
+// var hasil1 = (bil1 * bil2);
+// var hasil2 = (bil1 / bil2);
+// var hasil3 = (bil1 - bil2);
+
+// console.log(hasil);
+// console.log(hasil1);
+// console.log(hasil2);
+// console.log(hasil3);
+
+///operator logika (|| &&)
+// benar ||  benar = true
+// benar ||  salah = true
+// salah ||  benar = true
+// salah ||  salah = false
+
+// benar && benar = true 
+// benar && salah = false 
+// salah && benar = false 
+// benar && salah = false
+
+// let a =14 == "14"
+// console.log(a);
+
+// let nilai = 80;
+// if(nilai > 70){
+//       console.log("selamat kamu lulus")
+// }else {
+//     console.log("kamu harus belajar lagi")
+// }
+
+// const nilai = 95;
+// if (nilai >=90 && nilai <= 100) {
+//    console.log (nilai + "nilai sangat memuaskan");
+// } else if(nilai >=80 && nilai <= 89){ 
+//     console.log (nilai + "nilai memuaskan");  
+// } else if(nilai >=70 && nilai <=79){
+//     console.log ( nilai + "nilai cukup");
+// }else{
+//   console.log ("nilai + kurang");
+
+// }
+
+
+
+// const email = "nabilaislamiah35@gmail.com";
+// const pass = "aku";
+
+// if(email === "nabilaislamiah35@gmail.com") {
+//     if ( pass === "aku"){
+//         console.log ("kamu berhasil login")
+//     }else{
+//         console.log ("password salah");
+//     }
+// }else{ 
+//     console.log ("email belum terdaftar");
+
+// }
+
+// switch case
+
+// const warna = 'hitam';
+
+// switch (warna) {
+//     case "hitam":
+//         console.log("warna hitam");
+//         break;
+//     case "hitam" :
+//         console.log("warna putih");
+//     break;
+//     default:
+//         console.log("tidak ada warna pilihan");
+//         break;
+
+       
+
+// }
+ 
+
+ var pendapatan = 300000;
+ var komisi, jasa, total;
+
+ if (pendapatan >= 0 && pendapatan <= 200000){
+     jasa = 10000;
+     komisi = 0.1* pendapatan;
+ }
+ else if (pendapatan <= 500000)
+ {
+    jasa = 200000;
+    komisi = 0.12*pendapatan;
+ }
+ else
+ {
+  jasa = 300000;
+  komisi = 0.2;
+ }
+ {
+  total = komisi + jasa;
+  console.log(jasa);
+  console.log(komisi);
+  console.log(total);
+ }
+
+
+   
+
+
+    
---
+++ rumus.js
@@ -0,0 +1,15 @@
+//mencari luas segitiga
+
+
+var alas = 12;
+var tinggi = 32;
+
+var hasil = 1/2 ( alas * tinggi);
+console.log(hasil);
+
+//luas lingkaran 
+var r = 12;
+var r1 = 23;
+
+var hasil = 22/7 * (r * r1);
+console.log(hasil);
